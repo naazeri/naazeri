cc448cde7d77c7a08e07755ae71ef79713433aa0
Reza Nazeri <r.nazeri.vitrin@gmail.com>
2025-01-06 03:49:11 +0330

    deploy new realease

--- README.md
+++
@@ -1,3 +0,0 @@
-# Nazeriland
-
-Feel free to explore and connect with me!
--- src/components/SocialIcons.jsx
+++
@@ -1,5 +0,0 @@
-function SocialIcons({ children }) {
-  return <div className="flex gap-6 xs:gap-8">{children}</div>;
-}
-
-export default SocialIcons;
